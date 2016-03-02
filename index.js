36ff5d62a219a461c8ba25cafe7873ce661c1799
airtoxin <airtoxin@icloud.com>
2016-03-03 02:26:57 +0900

    Add option: keep aspect ratio

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = remote.app;
 const Menu = remote.Menu;
 const MenuItem = remote.MenuItem;
 
+let keepAspectRatio = false;
 let menu = new Menu();
 menu.append(new MenuItem({
   label: 'ファイルを開く...',
@@ -15,6 +16,20 @@ menu.append(new MenuItem({
   label: 'yamadaを終了',
   click: () => app.quit()
 }));
+menu.append(new MenuItem({
+  label: 'アスペクト比を維持',
+  type: 'checkbox',
+  checked: keepAspectRatio,
+  click: () => {
+    keepAspectRatio = !keepAspectRatio;
+    console.log("@keepAspectRatio", keepAspectRatio);
+  }
+}));
+
+const getInlineImageStyle = () => {
+  const maxOption = keepAspectRatio ? 'max-' : '';
+  return `style="${maxOption}width: 100%; ${maxOption}height: 100%"`
+};
 
 window.addEventListener('contextmenu', e => {
   e.preventDefault();
@@ -24,6 +39,6 @@ window.addEventListener('contextmenu', e => {
 document.addEventListener('DOMContentLoaded', () => {
   const mainEl = document.querySelector('.main');
   ipcRenderer.on('image', (ev, data) => {
-    mainEl.innerHTML = `<img src='${JSON.parse(data)}'>`;
+    mainEl.innerHTML = `<img src='${JSON.parse(data)}' ${getInlineImageStyle()}>`;
   });
 });
